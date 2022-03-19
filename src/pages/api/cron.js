22f710ec71568700f1ca1538b3245bfccfbb1fb2
AppSystem <juniormelo271101@icloud.com>
2022-03-19 02:54:35 -0300

    commit

--- src/pages/api/cron.js
+++ src/pages/api/cron.js
@@ -2,13 +2,10 @@ import { format } from 'date-fns';
 import firebase from '../../auth/AuthConfig';
 
 export default async function handler(req, res) {
-  const {  } = req;
-
   const date = new Date();
+
   await firebase.firestore().collection('postback').doc('githubActions').set({
-    [format(date, 'dd_MM_yyyy')]: {
-      request: req
-    }
+    [format(date, 'dd_MM_yyyy')]: format(date, 'hh:mm')
   }, { merge: true });
 
   res.status(200).json({
