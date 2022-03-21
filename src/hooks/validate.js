92d3969f2ca3a360a91e593f60d5b2ec6401985e
AppSystem <juniormelo271101@icloud.com>
2022-03-21 14:05:14 -0300

    commit

--- src/hooks/validate.js
+++ src/hooks/validate.js
@@ -15,17 +15,17 @@ export function validateCPF(cpf) {
 		cpf == "99999999999")
 			return false;		
 	// Valida 1o digito	
-	add = 0;	
-	for (i=0; i < 9; i ++)		
+	let add = 0;	
+	for (let i=0; i < 9; i ++)		
 		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
+	let rev = 11 - (add % 11);	
 		if (rev == 10 || rev == 11)		
 			rev = 0;	
 		if (rev != parseInt(cpf.charAt(9)))		
 			return false;		
 	// Valida 2o digito	
 	add = 0;	
-	for (i = 0; i < 10; i ++)		
+	for (let i = 0; i < 10; i ++)		
 		add += parseInt(cpf.charAt(i)) * (11 - i);	
 	rev = 11 - (add % 11);	
 	if (rev == 10 || rev == 11)	
