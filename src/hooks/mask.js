74fb7ee4270a18072d67e03eefebda1271048c86
AppSystem <juniormelo271101@icloud.com>
2022-03-28 12:28:15 -0300

    commit

--- src/hooks/mask.js
+++ src/hooks/mask.js
@@ -77,7 +77,7 @@ export const maskPlaca = value => {
   return value;
 };
 
-// Aceita apenas que letras sejam digitadas
+// Aceita apenas que letras sejam digitadas 
 export const maskOnlyLetters = value => {
   return value.replace(/[0-9!@#¨$%^&*)(+=._-]+/g, "");
 };
