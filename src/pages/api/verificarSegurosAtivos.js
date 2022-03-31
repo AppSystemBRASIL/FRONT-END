fde46cb90312834636a74b2b0687722913a3d99e
AppSystem <juniormelo271101@icloud.com>
2022-03-31 09:45:37 -0300

    commit

--- src/pages/api/verificarSegurosAtivos.js
+++ src/pages/api/verificarSegurosAtivos.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
   .where('ativo', '==', true);
 
   if(req.query.corretora) {
-    console.log(req.query.corretora)
     ref = ref.where('corretora.uid', '==', req.query.corretora);
   }
 
