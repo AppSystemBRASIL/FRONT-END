90fe6e4e38f9a1536a826536125724a98554fc83
AppSystem <juniormelo271101@icloud.com>
2022-04-18 14:53:31 -0300

    commit

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
 export default function  App() {
-  const [device, setDevice] = useState(null);
-
   useEffect(() => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    if(userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i )) {
-      setDevice('IOS');
-    }else if(userAgent.match( /Android/i )) {
-      setDevice('ANDROID');
+    if(userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+      window.location.href = 'https://www.apple.com/br/app-store/';
+    }else if(userAgent.match(/Android/i)) {
+      window.location.href = 'https://play.google.com/store/apps';
     }else {
-      setDevice('DESKTOP')
+      alert('Acesse com seu celular!');
     }
   }, []);
 
-  return (
-    <>
-      {device}
-    </>
-  )
+  return null;
 }
