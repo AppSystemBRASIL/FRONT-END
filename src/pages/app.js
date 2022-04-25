561a136ab090b30bc97e344b59d504dd9ceab23b
AppSystem <juniormelo271101@icloud.com>
2022-04-25 15:59:14 -0300

    commit

--- src/pages/app.js
+++ src/pages/app.js
@@ -37,14 +37,10 @@ export default function  App({ corretora }) {
   return (
     <Head>
       <title>{corretora.razao_social}</title>
-      <link rel='icon' type='image/png' href={corretora.icon} sizes='192x192' />
-
       <meta property='og:title' content={corretora.razao_social} />
-      <meta property='og:url' content={corretora.site} />
       <meta property='og:description' content='Seguros custam menos que você imagina' />
-      <meta property='og:image' itemprop='image' content={corretora.icon}/>
-      <meta property='og:image:width' content='300' />
-      <meta property='og:image:height' content='300' />
+      <meta property='og:url' content={corretora.site} />
+      <meta property='og:image' itemprop='image' content={corretora.icon} />
       <meta property='og:type' content='article' />
       <meta property='og:locale' content='pt_BR' />
     </Head>
