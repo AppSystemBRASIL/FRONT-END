0c41a9639289ff6f481daedd9f70176234199ec4
juniormelo27 <juniormelo271101@icloud.com>
2022-05-31 16:38:42 -0300

    commit

--- src/pages/painel/produtores.js
+++ src/pages/painel/produtores.js
@@ -15,6 +15,8 @@ import axios from 'axios';
 
 import { useTheme } from 'styled-components';
 
+import banco from '../../data/bancos';
+
 const Seguro = () => {
   const { user, corretora, setCollapsedSideBar, businessInfo } = useAuth();
 
@@ -44,7 +46,13 @@ const Seguro = () => {
     cidade: null,
     estado: null,
     cep: null,
-    comissao: 100 - businessInfo.comissao.percentual
+    comissao: 100 - businessInfo.comissao.percentual,
+    banco: null,
+    agencia: null,
+    agencia_d: null,
+    conta: null,
+    conta_d: null,
+    pix: null
   });
 
   useEffect(() => {
@@ -57,7 +65,13 @@ const Seguro = () => {
       cidade: null,
       estado: null,
       cep: null,
-      comissao: 100 - businessInfo.comissao.percentual
+      comissao: 100 - businessInfo.comissao.percentual,
+      banco: null,
+      agencia: null,
+      agencia_d: null,
+      conta: null,
+      conta_d: null,
+      pix: null
     })
   }, [viewNewSeguro]);
 
@@ -80,6 +94,14 @@ const Seguro = () => {
   }, [dataNewSeguro.cep]);
 
   const salvarSeguro = async () => {
+    if(!dataNewSeguro.nome || !dataNewSeguro.cpf || !dataNewSeguro.telefone || !dataNewSeguro.comissao) {
+      notification.error({
+        message: 'PREENCHA OS DADOS OBRIGATÓRIOS'
+      });
+
+      return;
+    }
+
     await firebase.firestore().collection('usuarios').add({
       corretora: {
         uid: corretora.uid,
@@ -94,7 +116,15 @@ const Seguro = () => {
       telefone: dataNewSeguro.telefone,
       tipo: 'corretor',
       created: new Date(),
-      comissao: dataNewSeguro.parcelas || []
+      comissao: dataNewSeguro.parcelas || [],
+      dadosBancarios: {
+        banco: dataNewSeguro.banco || null,
+        agencia: dataNewSeguro.agencia || null,
+        agencia_d: dataNewSeguro.agencia_d || null,
+        conta: dataNewSeguro.conta || null,
+        conta_d: dataNewSeguro.conta_d || null,
+        pix: dataNewSeguro.pix || null
+      }
     })
     .then((response) => {
       firebase.firestore().collection('usuarios').doc(response.id).set({ uid: response.id }, { merge: true });
@@ -114,17 +144,22 @@ const Seguro = () => {
     <LayoutAdmin title='PRODUTORES'>
       <CardComponent>
         <Modal onOk={salvarSeguro} title='NOVO PRODUTOR' cancelText='FECHAR' okText='SALVAR' onCancel={() => setViewNewSeguro(false)} visible={viewNewSeguro} closable={() => setViewNewSeguro(false)} style={{ top: 10 }} width='50%' cancelButtonProps={{ style: { border: '1px solid black', outline: 'none', color: 'black' } }} okButtonProps={{ style: { background: theme.colors[businessInfo.layout.theme].primary, border: 'none' }}}>
+          <Row>
+            <Col span={24}>
+              <h3>DADOS PESSOAIS:</h3>
+            </Col>
+          </Row>
           <Row gutter={[10, 20]}>
             <Col span={8}>
-              <label>NOME:</label>
+              <label>NOME: <sup style={{ color: 'red' }}>*</sup></label>
               <Input type='text' placeholder='NOME COMPLETO' value={dataNewSeguro.nome} onChange={value => setDataNewSeguro(e => ({...e, nome: maskOnlyLetters(value.target.value.toUpperCase())}))} />
             </Col>
             <Col span={8}>
-              <label>CPF:</label>
+              <label>CPF: <sup style={{ color: 'red' }}>*</sup></label>
               <Input type='tel' placeholder='000.000.000-00' value={dataNewSeguro.cpf} onChange={value => setDataNewSeguro(e => ({...e, cpf: maskCPF(value.target.value)}))} />
             </Col>
             <Col span={8}>
-              <label>CELULAR:</label>
+              <label>CELULAR: <sup style={{ color: 'red' }}>*</sup></label>
               <Input type='tel' placeholder='(00) 00000-0000' value={dataNewSeguro.telefone} onChange={value => setDataNewSeguro(e => ({...e, telefone: maskPhone(value.target.value)}))} />
             </Col>
             <Col span={24}>
@@ -133,14 +168,58 @@ const Seguro = () => {
             </Col>
           </Row>
           <br/>
-          <Row gutter={[10, 0]}>
+          <Row>
             <Col span={24}>
-              <h3>COMISSAO:</h3>
+              <h3>COMISSAO: <sup style={{ color: 'red' }}>*</sup></h3>
             </Col>
+          </Row>
+          <Row gutter={[10, 20]}>
             <Col>
-              <InputNumber value={dataNewSeguro.comissao} style={{ width: '100%' }} max={100} min={0}
-                onChange={value => setDataNewSeguro(e => ({ ...e, comissao: value }))}
+              <InputNumber value={dataNewSeguro.comissao} placeholder='0' style={{ width: '100%' }} max={100} min={0}
+                onChange={value => setDataNewSeguro(e => ({ ...e, comissao: value ? value : 0 }))}
               />
+            </Col>
+          </Row>
+          <br/>
+          <Row>
+            <Col span={24}>
+              <h3>DADOS BANCÁRIOS:</h3>
+            </Col>
+          </Row>
+          <Row gutter={[10, 20]}>
+            <Col span={8}>
+              <label>BANCO:</label>
+              <Select style={{ width: '100%' }}
+                showSearch
+                placeholder='SELECIONAR BANCO'
+                optionFilterProp='children'
+                onChange={value => setDataNewSeguro(e => ({...e, banco: String(value)}))}
+                value={dataNewSeguro.banco}
+              >
+                {banco.map((item, index) => (
+                  <Select.Option key={index} value={String(item.value)}>
+                    {item.value} - {item.label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Col>
+            <Col span={8}>
+              <label>AGÊNCIA:</label>
+              <Input.Group compact>
+                <Input value={dataNewSeguro.agencia} style={{ width: '80%' }} placeholder='AGÊNCIA' onChange={value => setDataNewSeguro(e => ({...e, agencia: value}))} />
+                <Input value={dataNewSeguro.agencia_d} style={{ width: '20%' }} placeholder='D' onChange={value => setDataNewSeguro(e => ({...e, agencia_d: value}))} />
+              </Input.Group>
+            </Col>
+            <Col span={8}>
+              <label>CONTA:</label>
+              <Input.Group compact>
+                <Input value={dataNewSeguro.conta} style={{ width: '80%' }} placeholder='CONTA' onChange={value => setDataNewSeguro(e => ({...e, conta: value}))} />
+                <Input value={dataNewSeguro.conta_d} style={{ width: '20%' }} placeholder='D' onChange={value => setDataNewSeguro(e => ({...e, conta_d: value}))} />
+              </Input.Group>
+            </Col>
+            <Col span={24}>
+              <label>PIX:</label>
+              <Input value={dataNewSeguro.pix} placeholder='PREENCHA COM O PIX PARA RECEBIMENTO' onChange={value => setDataNewSeguro(e => ({...e, pix: value}))} />
             </Col>
           </Row>
         </Modal>
