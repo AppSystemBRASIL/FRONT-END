ca265dc8c56afc0291b1f71f034dd01d240b6e80
Junior Melo <juniormelo271101@icloud.com>
2022-09-08 15:06:18 -0400

    commit

---
+++ src/pages/privacy.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export default function Privacy() {
+  useEffect(() => {
+    window.location.href = 'https://www.iubenda.com/privacy-policy/41982700';
+  }, []);
+
+  return null;
+}
